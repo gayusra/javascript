8eb88aac976ab9b8dc1255e5ed2e69594a1867d6
gayusra <dhana.gayathri100@gmail.com>
2024-07-24 22:58:47 -0700

    first commit

---
+++ Date/date.js
@@ -0,0 +1,19 @@
+/*const date =  new Date()
+console.log(date)
+console.log(date.getDay())
+console.log(date.getFullYear())*/
+
+
+
+//in js by default days starts from sunday(0) and ends with sat(7)
+
+const t = new Date()
+console.log(t)
+console.log('date ' + t.getDate())
+console.log('day ' + t.getDay())
+console.log('year ' + t.getFullYear())
+console.log('hours '+t.getHours())
+console.log('month ' + t.getMonth())// jan- 0 dec-11
+console.log('time '+ t.getTime())
+
+
