27123b3283d9fed0639626e470d467e70b59754e
gayusra <dhana.gayathri100@gmail.com>
2024-07-25 10:44:50 -0700

    first commit

---
+++ Literals/templete.js
@@ -0,0 +1,12 @@
+// template literals -- introduced in 2015 with or ES6
+
+let fname = 'Ram'
+let lname = 'babu'
+let city = 'chennai'
+//console.log(fname + ' ' + lname + 'lives in ' + city )//using this for so difficult
+
+
+//another method -- template literals
+let msg = `${fname} ${lname} lives in ${city}` //using back tick and doller
+console.log(msg)
+
---
+++ events/events.js
@@ -0,0 +1,16 @@
+
+/*   what is events?
+   in general events are triggering point for any action 
+   if software word when user submits a form opens an application html has events enables js code to be executed
+
+   html events 
+   onload -- browser loaded a page
+   onclick -- click on the document
+   ondblclick -- double click on the element
+   onmouseover -- mouseover on element
+   onmouseout -- mouse away from the element
+   onkeydown -- pushing keys an keyboard
+
+
+*/
+
